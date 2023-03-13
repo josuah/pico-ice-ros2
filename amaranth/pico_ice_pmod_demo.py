8650f2d3b08907c39eb8e3a8b5f1e18084ac50e0
Josuah Demangeon <me@josuah.net>
2023-03-13 23:58:57 +0100

    split the SPI module out of the main elaborated module

--- amaranth/pico_ice_pmod_demo.py
+++ amaranth/pico_ice_pmod_demo.py
@@ -4,43 +4,48 @@ from amaranth_boards.extensions.pmod import *
 from amaranth_boards.resources import *
 from pico_ice import PicoIcePlatform
 
-class PicoIcePmodDemo(Elaboratable):
-    def elaborate(self):
-        spi = platform.request("spi", 1)
-        led_r = platform.request("led_r", 0)
-        led_g = platform.request("led_g", 0)
-        led_b = platform.request("led_b", 0)
+class SPIPeripheral(Elaboratable):
+    def __init__(self, spi):
+        self.shift_buf = Signal(8)
+        self.shift_stb = Signal(1)
+        self.spi = spi
 
+    def elaborate(self, platform):
         m = Module()
-
         m.domains += ClockDomain("spi")
-        m.d.comb += ClockSignal("spi").eq(spi.clk)
+        m.d.comb += ClockSignal("spi").eq(self.spi.clk)
 
-        shift_buf = Signal(8)
         shift_num = Signal(3)
-        shift_stb = Signal(1)
 
         with m.FSM(domain="spi") as fsm:
 
             with m.State("START"):
-                m.d.spi += shift_buf[0].eq(spi.copi)
+                m.d.spi += self.shift_buf[0].eq(self.spi.copi)
                 m.d.spi += shift_num.eq(7)
                 m.next = "RX"
 
             with m.State("RX"):
-                m.d.spi += shift_buf.eq(Cat(shift_buf[1:-1], spi.copi))
+                m.d.spi += self.shift_buf.eq(Cat(self.shift_buf[1:-1], self.spi.copi))
                 m.d.spi += shift_num.eq(shift_num - 1)
                 with m.If(shift_num - 1 == 0):
-                    m.d.comb += shift_stb.eq(1)
+                    m.d.comb += self.shift_stb.eq(1)
                     m.next = "START"
 
-        with m.If(~spi.cs):
+        with m.If(~self.spi.cs):
             m.d.spi += fsm.state.eq(fsm.encoding["START"])
 
-        m.d.comb += spi.cipo.eq(fsm.ongoing("START"))
-        m.d.comb += spi.cipo.oe.eq(spi.cs)
+        m.d.comb += self.spi.cipo.eq(fsm.ongoing("START"))
+        m.d.comb += self.spi.cipo.oe.eq(self.spi.cs)
 
         return m
+
+class PicoIcePmodDemo(Elaboratable):
+    def elaborate(self):
+        spi = platform.request("spi", 1)
+
+        m = Module()
+
+        return SPIPeripheral(spi)
 
 if __name__ == "__main__":
     platform = PicoIcePlatform()
