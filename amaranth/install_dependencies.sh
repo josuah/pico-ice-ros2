5ccb77725020aefd584d7bcb6b891907de9dcee3
Josuah Demangeon <me@josuah.net>
2023-03-14 15:26:52 -0400

    add a more complete install instruction

--- amaranth/install_dependencies.sh
+++ amaranth/install_dependencies.sh
@@ -1,3 +1,5 @@
+set -eux
+
 # download the latest development packages
 pip3 install "amaranth[builtin-yosys] @ git+https://github.com/amaranth-lang/amaranth.git"
 pip3 install "amaranth-boards[builtin-yosys] @ git+https://github.com/amaranth-lang/amaranth-boards.git"
@@ -6,7 +8,6 @@ pip3 install "amaranth-boards[builtin-yosys] @ git+https://github.com/amaranth-l
 sudo apt-get install yosys dfu-util nextpnr-ice40 
 
 # icestorm is not packaged very often
-trap 'rm -rf /tmp/icestorm' INT TERM EXIT HUP
 git clone https://github.com/cliffordwolf/icestorm.git /tmp/icestorm
-make -C /tmp/icestorm
-sudo make -C /tmp/icestorm install
+make -C /tmp/icestorm/icepack/
+sudo cp /tmp/icestorm/icepack/icepack /usr/local/bin/icepack
