2add9d8eed46508bf858d692c5b1f0f9c2bd577b
Josuah Demangeon <me@josuah.net>
2023-03-18 15:30:26 +0100

    working nec_ir_decoder

--- amaranth/nec_ir_decoder.py
+++ amaranth/nec_ir_decoder.py
@@ -1,6 +1,10 @@
 from amaranth import *
 from amaranth.sim import *
 
+
+__all__ = [ "NecIrDecoder" ]
+
+
 class PulseWidthDecoder(Elaboratable):
     """
     Measure the duration of high and low states of a PWM signal.
@@ -17,17 +21,15 @@ class PulseWidthDecoder(Elaboratable):
     def elaborate(self, platform):
         m = Module()
         last_rx = Signal(1)
-        cycles  = Signal(self.data.shape())
 
         m.d.sync += last_rx.eq(self.rx)
-        m.d.comb += self.data.eq(cycles)
 
         # on falling edge
         with m.If(last_rx & ~self.rx):
-            m.d.sync += cycles.eq(0)
+            m.d.sync += self.data.eq(0)
             m.d.comb += self.en.eq(1)
         with m.Else():
-            m.d.sync += cycles.eq(cycles + 1)
+            m.d.sync += self.data.eq(self.data + 1)
 
         return m
 
@@ -57,9 +59,10 @@ class NecIrDecoder(Elaboratable):
         m.submodules.pwdec = pwdec = PulseWidthDecoder(width=32)
         m.d.comb += pwdec.rx.eq(self.rx)
 
-        idle_thres_ticks   = int(self.freq_hz * 1e-3)
-        sample_thres_ticks = int(self.freq_hz * (1.125e-3 + 2.25e-3) / 2)
-        prefix_thres_ticks = int(self.freq_hz * 1e-3)
+        # arbitrary values
+        prefix_thres_ticks = int(self.freq_hz * 9e-3)
+        sample_thres_ticks = int(self.freq_hz * 1.7e-3)
+        idle_thres_ticks   = int(self.freq_hz * 5e-3)
 
         m.d.comb += self.data.eq(sample_buf)
 
@@ -67,6 +70,8 @@ class NecIrDecoder(Elaboratable):
             with m.If(pwdec.data > idle_thres_ticks):
                 m.d.comb += self.err.eq(1)
                 m.next = "IDLE"
+
+        m.d.sync += self.en.eq(0)
 
         with m.FSM() as fsm:
 
@@ -92,15 +97,42 @@ class NecIrDecoder(Elaboratable):
 
         return m
 
+if __name__ == "__main__":
+    nul = "                                                     "
+    nul = nul + nul + nul
+    pfx = "################        "
+    pfx += "# # # # # # # # ### ### ### ### ### ### # ### "
 
-def test(name, stream):
+    rx = nul
+    rx += pfx + "# # ### ### # # # # ### ### # # ### ### ### ###" + nul # Num0
+    rx += pfx + "# # # # ### # # # ### ### ### ### # ### ### ###" + nul # Num1
+    rx += pfx + "### # # # ### # # # # ### ### ### # ### ### ###" + nul # Num2
+    rx += pfx + "# ### # # ### # # # ### # ### ### # ### ### ###" + nul # Num3
+    rx += pfx + "# # ### # ### # # # ### ### # ### # ### ### ###" + nul # Num4
+    rx += pfx + "### # ### # ### # # # # ### # ### # ### ### ###" + nul # Num5
+    rx += pfx + "# ### ### # ### # # # ### # # ### # ### ### ###" + nul # Num6
+    rx += pfx + "# # # ### ### # # # ### ### ### # # ### ### ###" + nul # Num7
+    rx += pfx + "### # # ### ### # # # ### ### # # # ### ### ###" + nul # Num8
+    rx += pfx + "# ### # ### ### # # # ### # ### # # ### ### ###" + nul # Num9
+    rx += pfx + "# ### ### ### # # # # ### # # # ### ### ### ###" + nul # Back
+    rx += pfx + "# ### # ### # # # # ### # ### # ### ### ### ###" + nul # Ch+
+    rx += pfx + "# # # ### # # # # ### ### ### # ### ### ### ###" + nul # Ch-
+    rx += pfx + "### # # ### # # # # # ### # ### ### ### ### ###" + nul # Enter
+    rx += pfx + "### # ### ### # # # # # ### # # ### ### ### ###" + nul # Next
+    rx += pfx + "### # # # # # # # # ### ### ### ### ### ### ###" + nul # Play
+    rx += pfx + "# ### ### # ### # # # ### # # ### # ### ### ###" + nul # Prev
+    rx += pfx + "# # ### # # # # # ### ### # ### ### ### ### ###" + nul # Setup
+    rx += pfx + "# ### # ### ### # # # ### # ### # # ### ### ###" + nul # Stop
+    rx += pfx + "# ### # # # # # # ### # ### ### ### ### ### ###" + nul # Vol+
+    rx += pfx + "# # # # # # # # ### ### ### ### ### ### ### ###" + nul # Vol-
+    rx += nul
+
     freq_hz = 2e4 # MHz
 
     dut = NecIrDecoder(freq_hz=freq_hz)
 
     def bench():
-        for ch in stream:
-            print(ch)
+        for ch in rx:
             for _ in range(int(freq_hz * 562.5e-6)):
                 yield
             yield dut.rx.eq(ch == "#")
@@ -108,34 +140,5 @@ def test(name, stream):
     sim = Simulator(dut)
     sim.add_clock(1/freq_hz)
     sim.add_sync_process(bench)
-    with sim.write_vcd("nec_ir_decoder.vcd"):
+    with sim.write_vcd(vcd_file=f"{__file__[:-3]}.vcd"):
         sim.run()
-
-
-if __name__ == "__main__":
-
-    rx = ""
-    rx += "################        "
-    rx += "# # # # # # # ### ### ### ### ### ### # ### "
-
-    test("0",     rx + "# # ### ### # # # # ### ### # # ### ### ### ###")
-    test("1",     rx + "# # # # ### # # # ### ### ### ### # ### ### ###")
-    test("2",     rx + "### # # # ### # # # # ### ### ### # ### ### ###")
-    test("3",     rx + "# ### # # ### # # # ### # ### ### # ### ### ###")
-    test("4",     rx + "# # ### # ### # # # ### ### # ### # ### ### ###")
-    test("5",     rx + "### # ### # ### # # # # ### # ### # ### ### ###")
-    test("6",     rx + "# ### ### # ### # # # ### # # ### # ### ### ###")
-    test("7",     rx + "# # # ### ### # # # ### ### ### # # ### ### ###")
-    test("8",     rx + "### # # ### ### # # # ### ### # # # ### ### ###")
-    test("9",     rx + "# ### # ### ### # # # ### # ### # # ### ### ###")
-    test("back",  rx + "# ### ### ### # # # # ### # # # ### ### ### ###")
-    test("ch+",   rx + "# ### # ### # # # # ### # ### # ### ### ### ###")
-    test("ch-",   rx + "# # # ### # # # # ### ### ### # ### ### ### ###")
-    test("enter", rx + "### # # ### # # # # # ### # ### ### ### ### ###")
-    test("next",  rx + "### # ### ### # # # # # ### # # ### ### ### ###")
-    test("play",  rx + "### # # # # # # # # ### ### ### ### ### ### ###")
-    test("prev",  rx + "# ### ### # ### # # # ### # # ### # ### ### ###")
-    test("setup", rx + "# # ### # # # # # ### ### # ### ### ### ### ###")
-    test("stop",  rx + "# ### # ### ### # # # ### # ### # # ### ### ###")
-    test("vol+",  rx + "# ### # # # # # # ### # ### ### ### ### ### ###")
-    test("vol-",  rx + "# # # # # # # # ### ### ### ### ### ### ### ###")
--- amaranth/pico_ice_board.py
+++ amaranth/pico_ice_board.py
@@ -1,29 +1,35 @@
 import sys
-sys.path.insert(0, 'lib/pico-ice-sdk/amaranth')
+sys.path.insert(0, "../lib/pico-ice-sdk/amaranth")
 from amaranth import *
 from amaranth.build import *
 from amaranth.cli import main
 from pico_ice import *
 from pmod_7seg import *
+from nec_ir_decoder import *
 
 
 class PicoIceBoard(Elaboratable):
+    """
+    Top module to test each Pmod with real hardware.
+    """
 
-    def elaborate(self, platform):
+    def elaborate(self):
+        freq_hz = platform.default_clk_frequency
         led = platform.request("led_g", 0)
         debug = platform.request("debug", 0)
         irsensor = platform.request("irsensor", 0)
         pmod_7seg = platform.request("pmod_7seg", 0)
 
         m = Module()
-        rx_d1 = Signal(1)
         debug_byte = Signal(8)
 
-        p7seg = m.submodules.p7seg = Pmod7Seg(pmod_7seg, debug_byte)
-        irdec = m.submodules.irdec = NecIrDecoder(rx=irsensor.rx)
+        m.submodules.p7seg = p7seg = Pmod7Seg(pmod_7seg, debug_byte)
+
+        m.submodules.irdec = irdec = NecIrDecoder(freq_hz=freq_hz)
+        m.d.comb += irdec.rx.eq(irsensor.rx)
 
         with m.If(irdec.en):
-            m.d.sync += debug_byte.eq(irdec.data[0:8])
+            m.d.sync += debug_byte.eq(irdec.data[8:16])
 
         # Visual feedback of IR remote action with an LED
         m.d.comb += led.eq(irsensor.rx)
@@ -33,7 +39,6 @@ class PicoIceBoard(Elaboratable):
 
 
 if __name__ == "__main__":
-
     platform = PicoIcePlatform()
     platform.add_resources([
 
